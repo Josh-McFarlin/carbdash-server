1079e2f284ed2be1954213336c6a4bb34d9f346c
Josh McFarlin <josh@mcfarl.in>
2021-11-21 11:21:07 -0500

    Modify recent return type

--- src/functions/recent/recent.ts
+++ src/functions/recent/recent.ts
@@ -38,7 +38,17 @@ export const findRecent: APIGatewayProxyHandler = async (
     });
 
     return Response.success({
-      recent,
+      recent: recent.map((content) => ({
+        ...content,
+        data: {
+          ...content.data,
+          likedBy: content.data.likedBy
+            ? Object.values(content?.data?.likedBy).map((i) =>
+                i.ref.toString()
+              ) || []
+            : [],
+        },
+      })),
     });
   } catch (error) {
     return Response.error(
